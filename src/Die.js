8a76a4bae5b06f16cab0dba0841197741e6bbd28
Peter Williams <pedroeldiablo@yahoo.com>
2019-08-27 15:04:20 +0100

    animates dice and tracks state

--- src/Die.js
+++ src/Die.js
@@ -2,15 +2,37 @@ import React, { Component } from "react";
 import "./Die.css";
 
 class Die extends Component {
+  static defaultProps = {
+    numbersAsWords: ["one", "two", "three", "four", "five", "six"],
+    val: 6
+  };
+
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.props.handleClick(this.props.idx)
+  }
   render() {
+   const { numbersAsWords, locked, val, disabled, rolling } = this.props;
+
+    let classes = `Die fas fa-3x fa-dice-${ numbersAsWords[val - 1 ]} `;
+    if (locked) {
+      classes += "Die-locked ";
+    }
+    if (rolling) {
+      classes += "Die-rolling ";
+    }
+    
     return (
-      <button
-        className={"Die"}
-        style={{ backgroundColor: this.props.locked ? "grey" : "black" }}
-        onClick={() => this.props.handleClick(this.props.idx)}
-      >
-        {this.props.val}
-      </button>
+      <i
+        className={classes}
+        onClick={this.handleClick}
+        disabled={disabled}
+      />
+    
     );
   }
 }

bf6cc2358b342344b58a7d1bd52ac1e2e3e03007
Peter Williams <pedroeldiablo@yahoo.com>
2019-08-27 13:21:52 +0100

    prevents reasigning scores

--- src/Rules.js
+++ src/Rules.js
@@ -68,12 +68,13 @@ class FullHouse extends Rule {
 class SmallStraight extends Rule{
   evalRoll = dice => {
     const d = new Set(dice);
-    if(d.has(2) && d.has(3) && d.has(4) && (d.has(5) || d.has(1)))
+    if(d.has(2) && d.has(3) && d.has(4) && (d.has(5) || d.has(1))) {
       return this.score;
-
-    if(d.has(3) && d.has(4) && d.has(5) && (d.has(2) || d.has(6)))
+    }
+     
+    if(d.has(3) && d.has(4) && d.has(5) && (d.has(2) || d.has(6))) {
       return this.score;
-
+    }
     return 0;
   };
   // TODO
